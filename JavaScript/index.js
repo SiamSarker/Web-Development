6c570b52e121dbcebed71e7db0337f0c72262860
SiamSarker <sarkersiam2@gmail.com>
2021-03-01 05:49:12 +0600

    Event handeling

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -9,3 +9,22 @@ document.getElementById("h2").innerHTML = "Good Bye"
 // document.getElementById("p").innerHTML = 'This is Siam Sarker' # for id  . for class 
 
 document.querySelector("#pi").innerHTML = "Hello There";
+
+
+var myVar = document.querySelector("#pid");
+
+function myMessage1(){
+    myVar.innerHTML = 'Clieked 1';
+}
+function myMessage2(){
+        myVar.innerHTML = 'Clieked 2';
+}
+
+var myPic = document.querySelector("#pic");
+
+function myPicture1(){
+    myPic.src = "Images/siam1.jpg"
+}
+function myPicture2(){
+        myPic.src = "Images/siam2.jpg"
+}
