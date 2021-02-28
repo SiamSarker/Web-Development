b427d6e66618d1a2a3bfab073a046f5160cf5bed
SiamSarker <sarkersiam2@gmail.com>
2021-03-01 05:14:57 +0600

    calculator

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,2 +1,11 @@
+// getElementbyId()
+// getElementsbyTagName()
+// getElementdbyClassName()
+// querySelector()
 
+document.getElementById("h1").innerHTML = "Hello World"
+document.getElementById("h2").innerHTML = "Good Bye"
 
+// document.getElementById("p").innerHTML = 'This is Siam Sarker' # for id  . for class 
+
+document.querySelector("#pi").innerHTML = "Hello There";
