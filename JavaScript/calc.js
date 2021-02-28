1853bdc2c2677e28f884cb12d1597724729249d6
SiamSarker <sarkersiam2@gmail.com>
2021-03-01 02:59:01 +0600

    calculator

---
+++ JavaScript/calc.js
@@ -0,0 +1,14 @@
+
+var a = prompt('Enter your first number : ');
+var b = prompt('Enter your second number : ');
+
+// var num1 = 10;
+// var num2 = 8;
+
+num1 = parseInt(a, 10);
+num2 = parseInt(b, 10);
+
+var sum, sub;
+
+sum = num1 + num2;
+document.write(num1+" + "+num2+" = "+sum);
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,2 @@
-        // alert('Siam Sarker');
-        document.write('Siam Sarker<br>');
-        document.write('I love Bangladesh');
-        console.log("Hi there")
+
+
