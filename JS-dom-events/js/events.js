5c2305e92a19673d01b95ea1be9828a0df6d0008
siamsarker <sarkersiam2@gmail.com>
2023-02-25 21:29:08 +0600

    dom manupulation

---
+++ JS-dom-events/js/events.js
@@ -0,0 +1,12 @@
+console.log('This is separate JS file');
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
