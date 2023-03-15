6c92db01bae1fa753f099ccc0f3ac0b7b26f31bb
siamsarker <sarkersiam2@gmail.com>
2023-03-15 12:27:09 +0600

    Country API

--- api-example/js/country.js
+++ api-example/js/country.js
@@ -14,11 +14,27 @@ const printCoutryAll = countries => {
         countryDiv.innerHTML = `
             <p>Country Name: ${country.name.common}</p>
             <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+            <button onclick='countryDetails("${country.cca2}")'>Details</button>
         `;
         countryContainer.appendChild(countryDiv);
 
     });
 }
 
+const countryDetails = code => {
+    fetch(`https://restcountries.com/v3.1/alpha/${code}`)
+    .then(res => res.json())
+    .then(data => printCoutryDetails(data[0]))
+}
+
+
+const printCoutryDetails = country => {
+    const countryDiv = document.getElementById('countryDetails');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+            <p>Country Name: ${country.name.common}</p>
+            <img src="${country.flags.png}">
+        `;
+}
 
 countryAll()
