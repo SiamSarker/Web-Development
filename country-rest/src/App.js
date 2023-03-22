b8cb4f0cb8fe425f3fa3ddc5a44d4836a07cbb59
siamsarker <sarkersiam2@gmail.com>
2023-03-22 20:47:26 +0600

    React rest countries

--- country-rest/src/App.js
+++ country-rest/src/App.js
@@ -1,53 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <h1>Country Details</h1>
-      <section>
-        <CountryDetails></CountryDetails>
-        <CountryContainer></CountryContainer>
-      </section>
+      
+        <Countries/>
+
+        
+      
     </div>
   );
-}
-
-
-function CountryContainer() {
-  const [countries, setCoutries] = useState([]);
-
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCoutries(data));
-  }, [])
-
-  return (
-    <div id='countryContainer'>
-      {
-        countries.map(country =>
-          <div className='country'>
-            <p>Country Name: {country.name.common}</p>
-            <p>Capital: {country.capital ? country.capital[0] : 'No Capital'}</p>
-            <button onClick={() => CountryDetails(country)}>Details</button>
-          </div>)
-      }
-    </div>
-  )
-}
-
-function CountryDetails(props) {
-  console.log(props.name?.common);
-  return (
-    <div className='country'>      
-      <p>Country Name: {props?.name?.common && (
- <h1>{props.name.common}</h1>
-)}</p>   
-      <img src="{props.flags.png}" alt="" />
-    </div>
-  )
 }
 
 export default App;
